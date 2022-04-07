62a1acd27a28c1528127a95171d69e9582b83f28
Matthias Heinz <heinz.matthias.3@gmail.com>
2022-04-07 16:37:30 +0200

    Add working version for derived_class_2.

--- advanced/bin/advanced_example.c
+++ advanced/bin/advanced_example.c
@@ -1,14 +1,19 @@
 #include "abc_cptr.h"
 #include "derived_class1.h"
+#include "derived_class2.h"
 
 int main(void) {
     struct abstract_base_class_c_wrapper a;
+    struct abstract_base_class_c_wrapper b;
 
     create_derived_class_1_instance(&a);
+    create_derived_class_2_instance(&b);
 
     print_class_instance_using_c_wrapper(&a);
+    print_class_instance_using_c_wrapper(&b);
 
     delete_derived_class_1_instance(&a);
+    delete_derived_class_2_instance(&b);
 
     return 0;
 }
---
+++ advanced/derived_2/derived_class2.h
@@ -0,0 +1,17 @@
+#ifndef ADVANCED_DERIVED_2_DERIVED_CLASS2_H_
+#define ADVANCED_DERIVED_2_DERIVED_CLASS2_H_
+
+#include "abc_cptr.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void create_derived_class_2_instance(struct abstract_base_class_c_wrapper*);
+void delete_derived_class_2_instance(struct abstract_base_class_c_wrapper*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // ADVANCED_DERIVED_2_DERIVED_CLASS2_H_
